58b1ac116f3791797d25037922f35c0a7b536f08
amolbarkale <barkaleamol@gmail.com>
2025-07-26 15:30:32 +0530

    learn how to access user input.

---
+++ Assignments/personal_information_collector.py
@@ -0,0 +1,10 @@
+## Problem 1: Personal Information Collector
+
+name = input("Enter your full name:")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+hobby = input("Enter your hobby: ")
+
+print(f"Hello, {name}!")
+print(f"You are {age} years old and live in {city}.")
+print(f"In your free time, you enjoy {hobby}.")
